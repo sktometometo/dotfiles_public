e7e865bea65ad362f21111aec152ba961e0dfcff
Koki Shinjo <sktometometo@gmail.com>
2020-07-22 14:15:48 +0900

    update bashrc configuration

--- bashrc/setup_bashrc.sh
+++ bashrc/setup_bashrc.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
 
-FILEDIR="$(cd $(dirname $0); pwd)/../files"
+FILEDIR="$(cd $(dirname $0); pwd)"
 
-echo "FILEDIR":$FILEDIR
-echo "HOME:"$HOME
-
-# delete existing files
-if [ -e $HOME/.bashrc ]; then
-    rm -f  $HOME/.bashrc
+if [ $HOME/.bashrc ]; then
+    mv $HOME/.bashrc $HOME/.bashrc_backup
 fi
-if [ -e $HOME/.gitconfig ]; then
-    rm -f  $HOME/.gitconfig
-fi
-
-# create config bashrc file
-if [ ! -e $HOME/.bashrc_config ]; then
-    cp $FILEDIR/template/.bashrc_config ~/.bashrc_config
-fi
-
-# symbolic links
-ln -s $FILEDIR/bashrc/.bashrc        $HOME/.bashrc
-ln -s $FILEDIR/config/.gitconfig     $HOME/.gitconfig
+cp $FILEDIR/bashrc $HOME/.bashrc
+cat $FILEDIR/bashrc_main >> $HOME/.bashrc
